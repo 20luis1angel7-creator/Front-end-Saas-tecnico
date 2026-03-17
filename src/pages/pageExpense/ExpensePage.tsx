82cab67164a97eb52fb828bcbdc4465273e6e97b
luis angel <20luis1angel7@gmail.com>
2026-03-17 10:40:57 -0400

    cree ExpensePage y expenseType

---
+++ src/pages/pageExpense/ExpensePage.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import type { Expense } from "../../type/ExpenseType";
+
+
+
+function ExpensePage() {
+     const [expenses, setExpense] = useState<Expense[]>([])
+
+     useEffect(() => {
+        const getExpense = async () => {
+            const res = await fetch("http://localhost:3000/expenses")
+            const data: Expense[] = await res.json()
+            setExpense(data)
+        }
+    getExpense()
+    },[])
+
+    return (
+        <>
+            <h2>Expense page</h2>
+
+            {expenses.map((expense) => {
+                <div key={expense.id}>
+                    <h3>{expense.companyId}</h3>
+                    <h3>{expense.description}</h3>
+                    <h3>{expense.amount}</h3>
+                    {/*<h3>{expense.date}</h3>
+                    <h3>{expense.createdAt}</h3>*/}
+                </div>
+            })}
+        </>
+    )
+}
+export default ExpensePage
---
+++ src/type/ExpenseType.tsx
@@ -0,0 +1,15 @@
+
+
+export type Expense = {
+    id: string,
+    companyId: string;
+    /*type: ExpenseType;*/
+    description: string;
+    amount: number;
+    date: Date;
+    createdAt: Date;
+}
+
+export type ExpenseProps = {
+    expense: Expense
+}
