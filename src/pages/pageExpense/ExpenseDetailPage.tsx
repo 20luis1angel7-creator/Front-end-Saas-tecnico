d7332a79d760e59eb12c5589e8d458c3a5d338b3
luis angel <20luis1angel7@gmail.com>
2026-03-17 11:43:55 -0400

    cree expensedetail

---
+++ src/pages/pageExpense/ExpenseDetailPage.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import type { Expense } from "../../type/ExpenseType"
+
+
+
+
+function ExpenseDetail() {
+
+    const { id } = useParams()
+    const [expense, setExpense] = useState<Expense | null>(null)
+
+    useEffect(() => {
+        const getExpense = async () => {
+            const res = await fetch(`http://localhost:3000/expenses/${id}`)
+            const data = await res.json()
+            setExpense(data)
+        }
+    getExpense()
+    },[id])
+
+    return (
+        <>
+            <h2>expense detail</h2>
+            <p>expense {id}</p>
+
+            {expense && (
+                <div>
+                    <h3>{expense.description}</h3>
+                    <h3>{expense.amount}</h3>
+                    {/*<h3>{expense.date}</h3>
+                    <h3>{expense.createdAt}</h3>*/}
+                </div>
+            )}
+        </>
+    )
+}
+export default ExpenseDetail
